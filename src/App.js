05fb357e91fe304b091a519b8c4d63595fdf1749
olivialin21 <s110819023@stu.ntue.edu.tw>
2021-02-27 17:47:58 +0800

    fin

--- src/App.js
+++ src/App.js
@@ -1,69 +1,65 @@
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="container-fluid">
+    <div>
+      <div className="container">
         <header className="header">
-            <a href="index.html">
-                <img className="logo" src="images/logo.png" alt="GEGEU3U"/>
-            </a>
-            <div className="contact">
-                <a href="https://www.instagram.com/define_twoowl_3g/?hl=zh-tw" target="_blank">
-                    <img className="contact_icon" src="images/icn_ig.png" alt="ig"/>
-                </a>
-                <a href="https://www.facebook.com/groups/1517468085196003" target="_blank">
-                    <img className="contact_icon" src="images/icn_fb.png" alt="fb"/>
-                </a>
-                <a href="https://www.youtube.com/channel/UCmej5oymLBhl8X3TGrd7JKg/" target="_blank">
-                    <img className="contact_icon" src="images/icn_yt.png" alt="yt"/>
-                </a>
-            </div>
-            <div className="header_ripple">
-                <div className="HOVER">
-                    <span></span>
+            <h1 className="title">Vegetables</h1>
+            <div className="header_icn">
+                <img className="icn_fb" src="images/btn_facebook.png" alt="fb"/>
+                <img className="icn_ig" src="images/btn_instagram.png" alt="ig"/>
+                <div className="header_line"></div>
+                <div className="box_login">
+                  <p className="login">LOGIN</p>
+                </div>
+                <div className="box_join">
+                  <p className="join">JOIN</p>
                 </div>
             </div>
-            <div className="navbar_bg">
-                <nav className="navbar navbar-expand-lg postiion-sticky">
-                    <button className="navbar-toggler third-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent22" aria-controls="navbarSupportedContent22" aria-expanded="false" aria-label="Toggle navigation">
-                        <div className="animated-icon3"><span></span><span></span><span></span></div>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent22">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <a className="nav-link" href="index.html">HOME</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a href="#" className="nav-link">TOUR</a>
-                                <div className="dropdown-content">
-                                    <a href="tour.html">花蓮</a>
-                                    <a href="aboutUs.html">台南</a>
-                                </div>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link" href="#">LIFESTYLE</a>
-                                <div className="dropdown-content">
-                                    <a href="lifestyle.html">ㄍㄟㄍㄟの文</a>
-                                    <a href="lifestyle3.html">小於三の圖</a>
-                                </div>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="aboutUs.html">ABOUT US</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
         </header>
+
+        <nav className="nav">
+            <ul className="nav_list">
+                <li className="nav_list_item">Home</li>
+                <li className="nav_list_item">About Us</li>
+                <li className="nav_list_item">Vegetables</li>
+                <li className="nav_list_item">Online</li>
+                <li className="nav_list_item">Contact</li>
+            </ul>
+        </nav>
+        
+        <img className="main_picture" src="images/img_main_pumpkin.png" alt="pumpkin"/>
+
         <article className="article">
-            <div id="nanogallery2"></div>
+            <div className="vegetables">
+                <h2 className="vegetables_title">Vegetables</h2>  
+                <div className="vegetables_img">
+                    <img src="images/img_vegetables_pepper.png" alt="pepper"/>
+                    <img src="images/img_vegetables_carrot.png" alt="carrot"/>
+                    <img src="images/img_vegetables_corn.png" alt="corn"/>
+                </div>  
+            </div>
+
+            <div className="contact">
+                <h2 className="contact_title">Contact</h2>
+                <div className="contact_box">
+                    <p className="contact_word1">For any questions or suggestions about Vegetables, Vegetables Club or your online order you can contact Vegetables Customer Service by phone, email or post and we’ll be happy to help.</p>
+                    <div className="contact_line"></div> 
+                    <p className="contact_word2">
+                        E-mail : Vegetables@aaabbccc.com
+                        <br/>
+                        PHONE : +886-123-456-789
+                    </p>
+                </div>
+            </div>
+           
         </article>
-        <footer className="footer">
-            <h2 className="footer_ge3">ㄍㄟㄍㄟ小於三</h2>
-            <p className="footer_word">歡迎分享，請標明出處</p>
-            <p className="footer_word footer_margin">SINCE 2020</p>
-        </footer>
+      </div>
+      <footer className="footer">
+        <p className="footer_word">Copyrights 2017 Vegetables cise / Design by Vegetables</p>
+      </footer>
     </div>
   );
 }
