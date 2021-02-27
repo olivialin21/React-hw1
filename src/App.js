21a88a6f76059a33fd9ee49388bf9e950d193b64
olivialin21 <s110819023@stu.ntue.edu.tw>
2021-02-27 18:14:47 +0800

    test5

--- src/App.js
+++ src/App.js
@@ -3,63 +3,128 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <div className="container">
+    <div className="container-fluid">
         <header className="header">
-            <h1 className="title">Vegetables</h1>
-            <div className="header_icn">
-                <img className="icn_fb" src="images/btn_facebook.png" alt="fb"/>
-                <img className="icn_ig" src="images/btn_instagram.png" alt="ig"/>
-                <div className="header_line"></div>
-                <div className="box_login">
-                  <p className="login">LOGIN</p>
+            <a href="index.html">
+                <img className="logo" src="images/logo.png" alt="GEGEU3U"/>
+            </a>
+            <div className="contact">
+                <a href="https://www.instagram.com/define_twoowl_3g/?hl=zh-tw" target="_blank">
+                    <img className="contact_icon" src="images/icn_ig.png" alt="ig"/>
+                </a>
+                <a href="https://www.facebook.com/groups/1517468085196003" target="_blank">
+                    <img className="contact_icon" src="images/icn_fb.png" alt="fb"/>
+                </a>
+                <a href="https://www.youtube.com/channel/UCmej5oymLBhl8X3TGrd7JKg/" target="_blank">
+                    <img className="contact_icon" src="images/icn_yt.png" alt="yt"/>
+                </a>
+            </div>
+            <div className="header_ripple">
+                <div className="HOVER">
+                    <span></span>
                 </div>
-                <div className="box_join">
-                  <p className="join">JOIN</p>
-                </div>
+            </div>
+            <div className="navbar_bg">
+                <nav className="navbar navbar-expand-lg postiion-sticky">
+                    <button className="navbar-toggler third-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent22" aria-controls="navbarSupportedContent22" aria-expanded="false" aria-label="Toggle navigation">
+                        <div className="animated-icon3"><span></span><span></span><span></span></div>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent22">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="index.html">HOME</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a href="#" className="nav-link">TOUR</a>
+                                <div className="dropdown-content">
+                                    <a href="tour.html">花蓮</a>
+                                    <a href="aboutUs.html">台南</a>
+                                </div>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link" href="#">LIFESTYLE</a>
+                                <div className="dropdown-content">
+                                    <a href="lifestyle.html">ㄍㄟㄍㄟの文</a>
+                                    <a href="lifestyle3.html">小於三の圖</a>
+                                </div>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="aboutUs.html">ABOUT US</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         </header>
-
-        <nav className="nav">
-            <ul className="nav_list">
-                <li className="nav_list_item">Home</li>
-                <li className="nav_list_item">About Us</li>
-                <li className="nav_list_item">Vegetables</li>
-                <li className="nav_list_item">Online</li>
-                <li className="nav_list_item">Contact</li>
-            </ul>
-        </nav>
-        
-        <img className="main_picture" src="images/img_main_pumpkin.png" alt="pumpkin"/>
-
-        <article className="article">
-            <div className="vegetables">
-                <h2 className="vegetables_title">Vegetables</h2>  
-                <div className="vegetables_img">
-                    <img src="images/img_vegetables_pepper.png" alt="pepper"/>
-                    <img src="images/img_vegetables_carrot.png" alt="carrot"/>
-                    <img src="images/img_vegetables_corn.png" alt="corn"/>
-                </div>  
+        <article>
+            <div className="profile" id="profile">
+                <h1 className="profile_word">PROFILE</h1>
+                <picture>
+                    <source srcset="img/img_3g_L.jpg" media="(min-width:992px)"/>
+                    <img className="img_3g" src="img/img_3g_M.jpg" alt="img_3g"/>
+                </picture>
             </div>
-
-            <div className="contact">
-                <h2 className="contact_title">Contact</h2>
-                <div className="contact_box">
-                    <p className="contact_word1">For any questions or suggestions about Vegetables, Vegetables Club or your online order you can contact Vegetables Customer Service by phone, email or post and we’ll be happy to help.</p>
-                    <div className="contact_line"></div> 
-                    <p className="contact_word2">
-                        E-mail : Vegetables@aaabbccc.com
-                        <br/>
-                        PHONE : +886-123-456-789
-                    </p>
+            <div className="intro">
+                <div className="animate__animated animate__slideInLeft intro_box">
+                    <span className="intro_img">
+                        <span className="intro_img_inner">
+                            <img className="intro_img_f" src="images/gege_f.jpg" alt="gege_front"/>
+                            <img className="intro_img_b" src="images/gege_b.png" alt="gege_back"/>
+                        </span>
+                    </span>
+                    <div className="intro_text">
+                        <h1 className="intro_title">ㄍㄟㄍㄟ</h1>
+                        <div>
+                            <p className="intro_word">□□我是ㄍㄟㄍㄟ，幸會幸會！｡:.ﾟヽ(`g ´)ﾉﾟ.:｡<br/>
+                                <br/>
+                                時間，不能重演，<br/>
+                                別將自己的夢想在腦中轉的殷切，<br/>
+                                所以這天，我突圍，<br/>
+                                去打破意志週期的輪迴，<br/>
+                                就算路途旁的聲響無可避免。<br/>
+                                期盼遇見…I am the man, who just wanna make some friends. Maybe YOU can.<br/>
+                                <br/>
+                                ㄜ……對，我只是把之前寫的改ㄌ些…<br/>
+                                至於遇見…我想…已經有解…((被盯 Σ(`Д ´;<br/>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="animate__animated animate__slideInRight intro_box shan_box">
+                    <div className="intro_text shan_text">
+                        <h1 className="intro_title">小於三</h1>
+                        <div>
+                            <p className="intro_word">
+                                □□這裡是魚山 a.k.a 小於三<br/>
+                                貼文的附圖大部分都是我畫的，<br/>
+                                在ㄍㄟㄍㄟ的心靈雞湯洗禮之間(?<br/>
+                                會不定時跟大家分享我們的有(ㄅㄚˋ)趣(ㄌㄧㄥˊ)日常٩(´8` *)و！<br/>
+                                <br/>
+                                大概是醬，那我先去找ㄍㄟㄍㄟ了～(๑´8`๑)/<br/>
+                            </p>
+                        </div>
+                    </div>
+                    <span className="intro_img">
+                        <span className="intro_img_inner">
+                            <img className="intro_img_f" src="images/shan_f.jpg" alt="shan_front"/>
+                            <img className="intro_img_b" src="images/shan_b.png" alt="shan_back"/>
+                        </span>
+                    </span>
                 </div>
             </div>
-           
         </article>
-      </div>
-      <footer className="footer">
-        <p className="footer_word">Copyrights 2017 Vegetables cise / Design by Vegetables</p>
-      </footer>
+        <div className="contact_us">
+            <div className="contact_us_width">
+                <h3 className="animate__animated animate__tada contact_word d-flex justify-content-center">
+                    <strong>Contact&nbsp; us</strong>
+                </h3>    
+            </div>
+        </div>
+        <footer className="footer">
+            <h2 className="footer_ge3">ㄍㄟㄍㄟ小於三</h2>
+            <p className="footer_word">歡迎分享，請標明出處</p>
+            <p className="footer_word footer_margin">SINCE 2020</p>
+        </footer>
     </div>
   );
 }
